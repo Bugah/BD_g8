b13848be7d5650a4681f2655cb3d928297a6263d
Fábio Perez <fabiovmp@gmail.com>
2012-11-12 09:41:06 -0200

    Busca tags relacionadas na inserção de imagens

---
+++ FindItGitEstavel/builtIn/upload_new.php
@@ -0,0 +1,127 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+	"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<style type="text/css">
+		@import "../style.css"
+	</style>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv="Content-Language" content="en" />
+	<meta name="GENERATOR" content="PHPEclipse 1.2.0" />
+	<title>title</title>
+</head>
+<body>
+
+<div class="center">
+<div id="upInfo" style="background-color:#FFFFAA;padding:5px;margin-right:10px;float:left;">
+<?php
+$allowedExts = array("jpg", "jpeg", "gif", "png");
+$extension = end(explode(".", $_FILES["file"]["name"]));
+if ((($_FILES["file"]["type"] == "image/gif")
+|| ($_FILES["file"]["type"] == "image/jpeg")
+|| ($_FILES["file"]["type"] == "image/jpg")
+|| ($_FILES["file"]["type"] == "image/png")
+|| ($_FILES["file"]["type"] == "image/pjpeg"))
+&& ($_FILES["file"]["size"] < 200000)
+&& in_array($extension, $allowedExts))
+  {
+  if($_POST["titulo"]) {
+  	// Validando a entrada. Evitando Injections com '.', ',', '/', '\', '|'
+  	$carac = array(".",",","/","\\","|");						// caracteres ilegais
+  	$_POST["titulo"] = str_replace($carac,"",$_POST["titulo"]); // retirando caracteres
+  	$tok = strtok($_FILES["file"]["type"],"/"); 				// procurando a extensão original do arquivo
+  	$_FILES["file"]["name"] = $_POST["titulo"].".".strtok("");
+  	?><h3><?php
+  	echo "Novo título: <i>".$_POST["titulo"]."</i>.";
+  	?></h3><?php
+  }
+
+	// Se ocorrer erro no upload  	
+  if ($_FILES["file"]["error"] > 0)
+    {
+    echo "Error: " . $_FILES["file"]["error"] . "<br />";
+    }
+	// Erro de tipo de entrada
+  else
+    {
+    ?><i><?php
+    echo "Upload: " . $_FILES["file"]["name"] . "<br />";
+    echo "Tipo: " . $_FILES["file"]["type"] . "<br />";
+    echo "Tamanho: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
+    echo "Localizado em: " . $_FILES["file"]["tmp_name"]. " <br /><br />";
+    ?></i><?php
+    }
+    if (file_exists("upload/" . $_FILES["file"]["name"]))
+      {
+      echo $_FILES["file"]["name"] . " já existe. ";
+      }
+    else
+      {
+      move_uploaded_file($_FILES["file"]["tmp_name"],
+      "upload/" . $_FILES["file"]["name"]);
+      
+	  /******************************************************************************/
+	  // Criando o descritor de imagens no formato .info através do script criaInfo.sh
+      error_reporting(E_ALL);
+
+		/* Add redirection so we can get stderr. */
+		echo '/bin/sh criaInfo.sh "'.$_REQUEST['desc']. '" "upload/' .substr($_FILES["file"]["name"], 0, -4). '" 2>&1';
+		//echo "<br />";
+		$handle = popen('/bin/sh criaInfo.sh "'.$_REQUEST['desc']. '" "upload/' .substr($_FILES["file"]["name"], 0, -4). '" 2>&1', 'r');
+		//$handle = popen('/bin/pwd', 'r');
+		//echo "'$handle'; " . gettype($handle) . "\n";
+		//$read = fread($handle, 2096);
+		//echo $read;
+		pclose($handle);
+	  /******************************************************************************/
+	  
+      echo "Local do arquivo: " . "upload/" . $_FILES["file"]["name"];
+      }
+	  ?>
+	 	<h2>Visualização da Imagem:</h2>
+		<img style="max-width:70%;" src="<?php echo "upload/".$_FILES["file"]["name"]; ?>" />
+		
+		<h2>Descrição da Imagem:</h2>
+		<?php
+		if(file_exists("upload/".substr($_FILES["file"]["name"], 0, -4).".info")) {
+			$lines = file("upload/".substr($_FILES["file"]["name"], 0, -4).".info");
+		
+		// Loop through our array, show HTML source as HTML source; and line numbers too.
+			foreach ($lines as $line_num => $line) {
+			    echo htmlspecialchars($line);
+			}
+		} else {
+			echo "<h3 style='color:red;'>Imagem sem descrição</h3>";
+		}
+		?>
+		</div>
+<?php
+  }
+	else
+  {
+  	echo "<h2>Erro</h2>";
+	echo $_FILES["file"]["name"];
+  	if ((($_FILES["file"]["type"] != "image/gif")
+	|| ($_FILES["file"]["type"] != "image/jpeg")
+	|| ($_FILES["file"]["type"] != "image/png")
+	|| ($_FILES["file"]["type"] != "image/pjpeg"))) 
+	{
+  		echo "Tipo de arquivo inválido. <br>".$_FILES["file"]["type"]." não é do tipo JPEG/PNG/GIF válido.";
+	}
+	else if(($_FILES["file"]["size"] > 200000)) {
+		echo "Tamanho inválido. ". ($_FILES["file"]["size"] / 1024)."Kb > 2MB.";
+	}
+	else {
+		echo "Erro desconhecido. ";
+	}
+  }
+?>
+<p><input type="button" value="Sair" onclick="window.location='../inserir.php'" /></p>
+</div>
+
+
+
+</div>
+
+</body>
+</html>
--- RelatedWords/related.rb
+++ RelatedWords/related.rb
@@ -1,8 +1,6 @@
 # coding: ISO-8859-1
 
 # Script para pegar tags relacionadas a partir de thesaurus.
-# Para mudar de site, é necessário descomentar linhas 16 e 25, 29-31 e comentar linhas 17 e 26.
-# (Poderia ter feito isso configurável...)
 
 require 'open-uri'
 require 'net/http'
@@ -11,31 +9,47 @@ require 'nokogiri'
 tag = ARGV[0]  
 
 if !tag then
-  puts "Comando invalido! Usagem: related.rb <tag>"
+  puts "Comando invalido! Usagem: ruby related.rb <tag>"
   exit(-1)
 end
 
-#url = "http://www.merriam-webster.com/thesaurus/"+tag
-url = "http://www.dicio.com.br/"+tag+"/"
-#url = "http://www.urbandictionary.com/thesaurus.php?term="+tag
+urls = [
+    "http://www.merriam-webster.com/thesaurus/"+tag,
+    "http://www.urbandictionary.com/thesaurus.php?term="+tag,
+    "http://www.dicio.com.br/"+tag+"/"
+]
+error_msg = [
+  "isn't in the thesaurus",
+  "find any related words",  
+  "foram encontradas palavras na busca por"
+]
+xpath_str = [
+  "(//div[@class='scnt']//span[@class='ssens'])[1]/div[position() <= 2]/a/text()",
+  "(//table[@id='entries']//td[@class='word'])/a/text()",
+  "//p[@class='adicional sinonimos']//a/text()"
+]
 
-begin
-  result = Nokogiri::HTML(open(url))
-  raise if (result.to_s.include? "find any related words") 
-rescue
-  puts "_Erro #{tag}"
-else
-  #related = result.xpath("(//div[@class='scnt']//span[@class='ssens'])[1]/div[2]/a/text()")
-  #related = result.xpath("(//table[@id='entries']//td[@class='word'])/a/text()")
-  related = result.xpath("//p[@class='adicional sinonimos']//a/text()")
+related = String.new()
 
-  related = related.to_a.uniq.join(",").downcase
-  related.gsub!(' [','')
-  related.gsub!('],','')
-  related.gsub!('[,','')
-  related.gsub!(' ','-')
-  related.gsub!(',',' ')
+urls.each_index do |i|
+  begin
+    result = Nokogiri::HTML(open(urls[i]))
+    raise if (result.to_s.include? error_msg[i]) 
+  rescue
+    #puts "_Erro #{tag} (service #{i})"
+  else
+    xresult = result.xpath(xpath_str[i])
 
-  puts "#{tag}: " + related
+    xresult = xresult.to_a.uniq.join(",").downcase
+    xresult.gsub!(' [','')
+    xresult.gsub!('],','')
+    xresult.gsub!('[,','')
+    xresult.gsub!(' ','-')
+    xresult.gsub!(',',' ')
+  
+    related << " " + xresult
+  end
 end
+
+puts "#{tag}: " + related
 
