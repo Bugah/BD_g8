d36d1b0d4b255805113bda218602b59ffa0db787
Guilherme <gagallo7@gmail.com>
2012-11-16 08:09:57 -0200

    Algumas mudanças no visual

--- FindItGit/cpTest/criaInfo.sh
+++
@@ -1,8 +0,0 @@
-#Criando os arquivos a partir das strings de entrada
-echo "$1" > $3.desc;
-echo "$2" > $3.tags;
-
-#Inserindo imagem no banco de dados
-echo $3
-echo ./tmp/tools/TableMaker/InsertMySQL 2 "$5" "$4" "$3".tags
-./tmp/tools/TableMaker/InsertMySQL 2 "$5" "$4" "$3".tags
